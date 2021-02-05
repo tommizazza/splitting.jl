e677f86b468baf6b0bc4a6cd9356c203d70b8425
giozazza <gio.zazzaretti@gmail.com>
2021-02-05 16:20:34 +0100

    aggiunti altri test per le funzioni di refactoring.jl

--- splitting/test/splitting.jl
+++ splitting/test/splitting.jl
@@ -2,6 +2,50 @@ import splitting
 using Test
 using OrderedCollections
 using BenchmarkTools
+using LinearAlgebraicRepresentation
+using IntervalTrees
+Lar = LinearAlgebraicRepresentation
+
+	@testset "boxcovering" begin
+		V = [0.0 1.0 0.0 0.0 -1.0 -1.0; 
+		     0.0 0.0 1.0 0.5 0.0 0.5]
+		CV = [[1,2],[1,3],[2,3],[4,5],[4,6],[5,6]]
+		cellpoints = [ V[:,CV[k]]::Lar.Points for k=1:length(CV) ]
+		#----------------------------------------------------------
+		bboxes = [hcat(splitting.boundingbox(cell)...) for cell in cellpoints]
+		xboxdict = splitting.coordintervals(1,bboxes)
+		yboxdict = splitting.coordintervals(2,bboxes)
+		# xs,ys are IntervalTree type
+		xs = IntervalTrees.IntervalMap{Float64, Array}()
+		for (key,boxset) in xboxdict
+			xs[tuple(key...)] = boxset
+		end
+		ys = IntervalTrees.IntervalMap{Float64, Array}()
+		for (key,boxset) in yboxdict
+			ys[tuple(key...)] = boxset
+		end
+		@test splitting.boxcovering(bboxes, 1, xs) == [[4, 5, 2, 1, 3], [4, 5, 2, 1, 3], [4, 5, 2, 1, 3], [6, 4, 5, 2, 1, 3], [6, 4, 5, 2, 1, 3], [6, 4, 5]]
+
+		V1 = [0.0 1.0; 
+		     0.0 0.0]
+		EV1 = [[1,2]]
+		cellpoints = [ V1[:,EV1[k]]::Lar.Points for k=1:length(EV1) ]
+		#----------------------------------------------------------
+		bboxes = [hcat(splitting.boundingbox(cell)...) for cell in cellpoints]
+		xboxdict = splitting.coordintervals(1,bboxes)
+		yboxdict = splitting.coordintervals(2,bboxes)
+		# xs,ys are IntervalTree type
+		xs = IntervalTrees.IntervalMap{Float64, Array}()
+		for (key,boxset) in xboxdict
+			xs[tuple(key...)] = boxset
+		end
+		ys = IntervalTrees.IntervalMap{Float64, Array}()
+		for (key,boxset) in yboxdict
+			ys[tuple(key...)] = boxset
+		end
+		@test splitting.boxcovering(bboxes, 1, xs) == [[1]]
+	end
+
 	@testset "coordintervals" begin
 		@test splitting.coordintervals(1, []) == OrderedDict{Array{Float64,1},Array{Int64,1}}() 
 		@test splitting.coordintervals(1, [[0.0 0.0; 0.0 0.0]]) == OrderedCollections.OrderedDict([0.0, 0.0] => [1])
@@ -45,9 +89,35 @@ using BenchmarkTools
 		
 	end
 
-	#TODO
-	@testset "congruence" begin
+	@testset "fragmentlines" begin
+		V = [0.0 1.0 0.0 1.0; 
+		     0.0 1.0 1.0 0.0]
+		EV = [[1,2],[3,4]]
+		a, b =  splitting.fragmentlines((V,EV)) 
+		@test a == [0.0 0.5 1.0 0.0 1.0; 0.0 0.5 1.0 1.0 0.0]
+		@test b == [[1, 2], [2, 3], [4, 2], [2, 5]]
+		V1 = [0.0 1.0 ; 
+		     0.0 0.0]
+		EV1 = [[1,2]]
+		a, b = splitting.fragmentlines((V1,EV1))
+		@test V1 == a
+		@test EV1 == b
 		
+	end
+
+	@testset "congruence" begin
+		V1 = [0.0 1.0 ; 
+		     0.0 0.0]
+		EV1 = [[1,2]]
+		a, b = splitting.congruence((V1,EV1))
+		@test a == V1
+		@test b == EV1
+
+		V1 = [0.0 1.0 0.99999999999999999 ; 
+		     0.0 0.0 0.0]
+		EV1 = [[1,2], [1,3]]
+		a, b = splitting.congruence((V1,EV1))
+		@test b == [[1, 2], [1, 2]] 
 		
 	end
 	
